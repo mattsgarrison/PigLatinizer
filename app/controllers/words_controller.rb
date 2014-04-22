344872727306a79cb7a81e679bdfd86a44be9966
Spatula <alejandro@roundpegg.com>
2014-04-21 21:26:08 -0600

    words_controller#new

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,6 +1,7 @@
 class WordsController < ApplicationController
 
   def new
+    @word = Word.new
   end
 
   def create
--- spec/words_controller.rb
+++ spec/words_controller.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe WordsController do
+
+  describe :index do
+    it "should render successfully" do
+      get :index
+      response.should be_successful
+    end
+
+    it "should init word object" do
+      assigns(:word).should be_a(Word)
+    end
+  end
 end
