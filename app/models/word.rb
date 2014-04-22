94137d53af82a586597cc51216a46054fea3f8c8
Spatula <alejandro@roundpegg.com>
2014-04-21 21:00:57 -0600

    Word: fix format validation regex

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,5 +2,5 @@ class Word < ActiveRecord::Base
   has_many :translations
 
   validates_presence_of :word
-  validates_format_of :word, with: /\A[a-zA-Z]\z/
+  validates_format_of :word, with: /\A[a-zA-Z]+\z/
 end
--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -1,14 +1,20 @@
 require 'spec_helper'
 
 describe Word do
-  it {should validate_presence_of(:word) }
-  it {should have_many(:translations) }
+  it { should validate_presence_of(:word) }
+  it { should have_many(:translations) }
 
   describe "format validations" do
     context "for word" do
       context "when format is invalid" do
         ["abc123", "123"].each do |invalid|
-          it {should_not allow_value(invalid).for(:word)}
+          it { should_not allow_value(invalid).for(:word) }
+        end
+      end
+
+      context "when format is valid" do
+        ["validword", "a"].each do |valid|
+          it { should allow_value(valid).for(:word) }
         end
       end
     end
