4301b8e0af7f440a86809731708b7ecab5b9609c
Spatula <alejandro@roundpegg.com>
2014-04-21 20:27:56 -0600

    PigLatinTranslator: add vowels array, add #translation

--- lib/pig_latin_translator.rb
+++ lib/pig_latin_translator.rb
@@ -1,4 +1,12 @@
 class PigLatinTranslator
-  def initialize
+  VOWELS = %w{A E I O U}
+  attr_accessor :word
+
+  def initialize(word=nil)
+    @word = word
+  end
+
+  def translation
+    return nil if !@word
   end
 end
--- spec/lib/pig_latin_translator_spec.rb
+++ spec/lib/pig_latin_translator_spec.rb
@@ -1,4 +1,14 @@
 require 'spec_helper'
 
-describe PigLatin do
+describe PigLatinTranslator do
+
+  let(:translator) { PigLatinTranslator.new }
+
+  describe :translation do
+    context "when word isn't set" do
+      it "should return nil" do
+        translator.translation.should be_nil
+      end
+    end
+  end
 end
