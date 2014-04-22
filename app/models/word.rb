2571dea5c7e15f57a8e7fd4b95b401a2d2ce99a4
Spatula <alejandro@roundpegg.com>
2014-04-21 21:53:14 -0600

    word: add uniqueness validation to word

--- app/models/word.rb
+++ app/models/word.rb
@@ -6,6 +6,7 @@ class Word < ActiveRecord::Base
   end
 
   validates_presence_of :word
+  validates_uniqueness_of :word
   validates_format_of :word, with: /\A[a-zA-Z]+\z/
 
   def add_translation(type)
