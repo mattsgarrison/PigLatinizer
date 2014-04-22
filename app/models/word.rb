b5810fe1ea060b70e168b44383cb643e9d54ca49
Spatula <alejandro@roundpegg.com>
2014-04-21 19:49:50 -0600

    word: validate word presence, format

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,5 @@
 class Word < ActiveRecord::Base
 
+  validates_presence_of :word
+  validates_format_of :word, with: /\A[a-zA-Z]\z/
 end
--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe Word do
+  it {should validate_presence_of(:word) }
+
+  describe "format validations" do
+    context "for word" do
+      context "when format is invalid" do
+        ["abc123", "123"].each do |invalid|
+          it {should_not allow_value(invalid).for(:word)}
+        end
+      end
+    end
+  end
 end
