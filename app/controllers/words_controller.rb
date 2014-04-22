c71ffffac8cdd1b19d58859a105cff9398c3bd9b
Spatula <alejandro@roundpegg.com>
2014-04-21 21:47:27 -0600

    words#create: implement to return translated word in flash or invalid message

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -5,5 +5,20 @@ class WordsController < ApplicationController
   end
 
   def create
+    word = Word.create(word_params)
+    if word.persisted?
+      pig_translation = word.add_translation(:pig_latin)
+      notice = "Your word in Pig Latin is #{pig_translation}"
+    else
+      notice = "Your word was invalid"
+    end
+
+    redirect_to root_path, notice: notice
+  end
+
+  private
+
+  def word_params
+    params.require(:word).permit(:word)
   end
 end
---
+++ spec/controllers/words_controller.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe WordsController do
+
+  describe :new do
+    it "should render successfully" do
+      get :new
+      response.should be_successful
+    end
+
+    it "should init word object" do
+      assigns(:word).should be_a(Word)
+    end
+  end
+
+  describe :create do
+    context "when word is invalid" do
+      it "should redirect to new word path" do
+        post :create, word: { word: nil }
+        response.should redirect_to new_word_path
+      end
+
+      it "should set error message in flash" do
+        post :create, word: { word: nil }
+        flash[:notice].should == "Your word was invalid"
+      end
+    end
+
+    context "when word is valid" do
+      it "should redirect to new word path" do
+        post :create, word: { word: "hi" }
+        response.should redirect_to new_word_path
+      end
+
+      it "should call add_translation on word with :pig_latin" do
+        Word.any_instance.expects(:add_translation).with(:pig_latin)
+        post :create, word: { word: "hi" }
+      end
+
+      it "should return pig latin translation in flash" do
+        post :create, word: { word: "hi" }
+        flash[:notice].should == "ihay"
+      end
+    end
+  end
+end
