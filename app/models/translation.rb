4e164dbd8c1742031f9a09cc7cd65b09bf6c075b
Spatula <alejandro@roundpegg.com>
2014-04-21 21:58:41 -0600

    translation: validate uniqueness of translated_word

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -2,4 +2,5 @@ class Translation < ActiveRecord::Base
   belongs_to :word
 
   validates_presence_of :translated_word
+  validates_uniqueness_of :translated_word
 end
--- spec/models/translation_spec.rb
+++ spec/models/translation_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 describe Translation do
   it { should belong_to(:word) }
   it { should validate_presence_of(:translated_word) }
+  it { should validate_uniqueness_of(:translated_word) }
 end
